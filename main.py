0d539c10d663852adc308e9083655f0d0ef1e3a6
ShorelineCrypto <admin@shorelinecrypto.com>
2024-02-24 19:33:44 -0500

    single pair, add CHTA/DGB

--- main.py
+++ main.py
@@ -73,14 +73,22 @@ def main():
     print (" NENG/DGB mkt price: {}\t DGB/NENG mkt price: {}".format(str(NENG_DGB_price), str(DGB_NENG_price)))
     CHTA_DGB_price = float(current_prices["CHTA"]["last_price"]) / float(current_prices["DGB"]["last_price"])
     DGB_CHTA_price = float(current_prices["DGB"]["last_price"]) / float(current_prices["CHTA"]["last_price"])
+    print (" CHTA/DGB mkt price: {}\t DGB/CHTA mkt price: {}".format(str(CHTA_DGB_price), str(DGB_CHTA_price)))
        
-    # trading pair min_usd = $0.05, NENG_unit CHTA_unit unchanged
+    ## trading pair min_usd = $0.05, NENG_unit CHTA_unit unchanged
     DGB_unit =  round ((0.05 / float(current_prices["DGB"]["last_price"])), 8)
 
-    # use new komododif scripts to support DGB-segwit
-    for i in range(1, 4):
-        spread = base_spread * i
+    ## use new komododif scripts to support DGB-segwit
+    ## the setprice will overwrite old order, one order pair only
+    ## setprice in place_order.sh is sell always
+    for j in range(1, 2):
+        spread = base_spread * j
     
+        print("./place_order.sh CHTA DGB-segwit {} {} | jq '.'".format((CHTA_DGB_price * (1 + spread)), CHTA_unit))
+        result = subprocess.run("./place_order.sh CHTA DGB-segwit {} {} | jq '.'".format((CHTA_DGB_price * (1 + spread)), CHTA_unit), shell=True)
+        print("./place_order.sh DGB-segwit CHTA {} {} | jq '.'".format((DGB_CHTA_price * (1 + spread)), DGB_unit))
+        result = subprocess.run("./place_order.sh DGB-segwit CHTA {} {} | jq '.'".format((DGB_CHTA_price * (1 + spread)), DGB_unit), shell=True)
+
         print("./place_order.sh NENG DGB-segwit {} {} | jq '.'".format((NENG_DGB_price * (1 + spread)), NENG_unit))
         result = subprocess.run("./place_order.sh NENG DGB-segwit {} {} | jq '.'".format((NENG_DGB_price * (1 + spread)), NENG_unit), shell=True)
         print("./place_order.sh DGB-segwit NENG {} {} | jq '.'".format((DGB_NENG_price * (1 + spread)), DGB_unit))
