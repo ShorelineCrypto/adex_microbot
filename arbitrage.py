ff85783eef4a3fc0e72825a160427e1069ea7df0
ShorelineCrypto <admin@shorelinecrypto.com>
2024-06-09 11:54:30 -0400

    simple version of arb bot

--- arbitrage.py
+++ arbitrage.py
@@ -39,34 +39,17 @@ from helpers import (
 
 def main():
     current_prices = get_prices()
+    USD_unit = 1.0
+    base_spread = 0.1
 
     NENG_KMD_price = float(current_prices["NENG"]["last_price"]) / float(current_prices["KMD"]["last_price"])
+    KMD_NENG_price = float(current_prices["KMD"]["last_price"]) / float(current_prices["NENG"]["last_price"])
     CHTA_KMD_price = float(current_prices["CHTA"]["last_price"]) / float(current_prices["KMD"]["last_price"])
+    KMD_CHTA_price = float(current_prices["KMD"]["last_price"]) / float(current_prices["CHTA"]["last_price"])
     print (" NENG/KMD mkt price: {}\t CHTA/KMD mkt price: {}".format(str(NENG_KMD_price), str(CHTA_KMD_price)))
-    # trading pair min_usd = $0.05
-    NENG_unit = round ((0.05 / float(current_prices["NENG"]["last_price"])), 4)
-    CHTA_unit = round ((0.05 / float(current_prices["CHTA"]["last_price"])), 4)
-    base_spread = 0.01
-    print ("/root/mmtools/cancel_all_orders")
-    result = subprocess.run("/root/mmtools/cancel_all_orders", shell=True)
-
-
-    for i in range(1, 4):
-        spread = base_spread * i
-        print("/root/mmtools/buy CHTA KMD {} {}".format((CHTA_KMD_price / (1 + spread)), CHTA_unit))
-        result = subprocess.run("/root/mmtools/buy CHTA KMD {} {}".format((CHTA_KMD_price / (1 + spread)), CHTA_unit), shell=True)
-        print("/root/mmtools/sell CHTA KMD {} {}".format((CHTA_KMD_price * (1 + spread)), CHTA_unit))
-        result = subprocess.run("/root/mmtools/sell CHTA KMD {} {}".format((CHTA_KMD_price * (1 + spread)), CHTA_unit), shell=True)
-    
-        print("/root/mmtools/buy NENG KMD {} {}".format((NENG_KMD_price / (1 + spread)), NENG_unit))
-        result = subprocess.run("/root/mmtools/buy NENG KMD {} {}".format((NENG_KMD_price / (1 + spread)), NENG_unit), shell=True)
-        print("/root/mmtools/sell NENG KMD {} {}".format((NENG_KMD_price * (1 + spread)), NENG_unit))
-        result = subprocess.run("/root/mmtools/sell NENG KMD {} {}".format((NENG_KMD_price * (1 + spread)), NENG_unit), shell=True)
-
-
-    ## start DGB pair on new MM2 scripts, mmtool not used
-    os.chdir('/root/atomicDEX-API/target/debug')
-    print("path changed to /root/atomicDEX-API/target/debug")
+    # trading pair min_usd = $1.0
+    NENG_unit = round ((USD_unit / float(current_prices["NENG"]["last_price"])), 4)
+    CHTA_unit = round ((USD_unit / float(current_prices["CHTA"]["last_price"])), 4)
 
     NENG_DGB_price = float(current_prices["NENG"]["last_price"]) / float(current_prices["DGB"]["last_price"])
     DGB_NENG_price = float(current_prices["DGB"]["last_price"]) / float(current_prices["NENG"]["last_price"])
@@ -75,9 +58,16 @@ def main():
     DGB_CHTA_price = float(current_prices["DGB"]["last_price"]) / float(current_prices["CHTA"]["last_price"])
     print (" CHTA/DGB mkt price: {}\t DGB/CHTA mkt price: {}".format(str(CHTA_DGB_price), str(DGB_CHTA_price)))
        
-    ## trading pair min_usd = $0.05, NENG_unit CHTA_unit unchanged
-    DGB_unit =  round ((0.05 / float(current_prices["DGB"]["last_price"])), 8)
+    DGB_unit = round ((USD_unit / float(current_prices["DGB"]["last_price"])), 8)
+    KMD_unit = round ((USD_unit / float(current_prices["KMD"]["last_price"])), 8)
+    
+    print ("/root/mmtools/cancel_all_orders")
+    result = subprocess.run("/root/mmtools/cancel_all_orders", shell=True)
 
+    ## start DGB pair on new MM2 scripts, mmtool not used
+    os.chdir('/root/atomicDEX-API/target/debug')
+    print("path changed to /root/atomicDEX-API/target/debug")
+    
     ## use new komododif scripts to support DGB-segwit
     ## the setprice will overwrite old order, one order pair only
     ## setprice in place_order.sh is sell always
@@ -93,6 +83,16 @@ def main():
         result = subprocess.run("./place_order.sh NENG DGB-segwit {} {} | jq '.'".format((NENG_DGB_price * (1 + spread)), NENG_unit), shell=True)
         print("./place_order.sh DGB-segwit NENG {} {} | jq '.'".format((DGB_NENG_price * (1 + spread)), DGB_unit))
         result = subprocess.run("./place_order.sh DGB-segwit NENG {} {} | jq '.'".format((DGB_NENG_price * (1 + spread)), DGB_unit), shell=True)
+
+        print("./place_order.sh CHTA KMD {} {} | jq '.'".format((CHTA_KMD_price * (1 + spread)), CHTA_unit))
+        result = subprocess.run("./place_order.sh CHTA KMD {} {} | jq '.'".format((CHTA_KMD_price * (1 + spread)), CHTA_unit), shell=True)
+        print("./place_order.sh KMD CHTA {} {} | jq '.'".format((KMD_CHTA_price * (1 + spread)), KMD_unit))
+        result = subprocess.run("./place_order.sh KMD CHTA {} {} | jq '.'".format((KMD_CHTA_price * (1 + spread)), KMD_unit), shell=True)
+
+        print("./place_order.sh NENG KMD {} {} | jq '.'".format((NENG_KMD_price * (1 + spread)), NENG_unit))
+        result = subprocess.run("./place_order.sh NENG KMD {} {} | jq '.'".format((NENG_KMD_price * (1 + spread)), NENG_unit), shell=True)
+        print("./place_order.sh KMD NENG {} {} | jq '.'".format((KMD_NENG_price * (1 + spread)), KMD_unit))
+        result = subprocess.run("./place_order.sh KMD NENG {} {} | jq '.'".format((KMD_NENG_price * (1 + spread)), KMD_unit), shell=True)
 
 
 if __name__ == "__main__":
