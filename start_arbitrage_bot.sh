92ddb92e8118cab6c60d6e22a820a83c439550c1
ShorelineCrypto <admin@shorelinecrypto.com>
2025-07-01 23:30:20 -0400

    allow slow nonkyc trade thru

--- start_arbitrage_bot.sh
+++ start_arbitrage_bot.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-   ## allow CEX nonkyc trade to finish in 10 minutes to avoid duplicate trades
-   timeout 600 python3 /opt/adex_microbot/arbitrage.py 2>&1 > /root/arb.log &
+   ## allow CEX nonkyc trade to finish in 30 minutes to avoid duplicate trades
+   timeout 1800 python3 /opt/adex_microbot/arbitrage.py 2>&1 >> /root/arb.log &
    sleep 180
 done
