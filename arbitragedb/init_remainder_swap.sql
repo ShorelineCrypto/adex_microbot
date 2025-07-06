d405005f43f373f4e1b04bf07fb32c2aca09b894
ShorelineCrypto <admin@shorelinecrypto.com>
2025-07-06 14:13:01 -0400

    init remainder swap table

---
+++ arbitragedb/init_remainder_swap.sql
@@ -0,0 +1,2 @@
+INSERT INTO remainder_swaps_arbitrage (coin,quantity,arb_price,arb_side)  VALUES ('NENG',0,0,'buy');
+INSERT INTO remainder_swaps_arbitrage (coin,quantity,arb_price,arb_side)  VALUES ('CHTA',0,0,'buy');
