9ebde5d846d70ef43a53fcf62bbc19e4e819b582
ShorelineCrypto <admin@shorelinecrypto.com>
2025-08-21 23:24:05 -0400

    update arm64 kdf v2.5.1-beta

--- mm2/update_mm2.sh
+++ mm2/update_mm2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm mm2
+rm mm2 kdf
 
 # For release binaries
 # wget $(curl -vvv https://api.github.com/repos/KomodoPlatform/komodo-defi-framework/releases | jq -r '.[0].assets | map(select(.name | contains("Linux-Release."))) | .[0].browser_download_url') -O mm2.zip
