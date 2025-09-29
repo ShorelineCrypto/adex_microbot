cbbdc44875d0c80bd57f41c6282070a7b8a9b755
ShorelineCrypto <admin@shorelinecrypto.com>
2025-09-29 12:59:03 -0400

    change str matching to float matching on quantity

--- arbitrage.py
+++ arbitrage.py
@@ -295,13 +295,13 @@ async def clear_cex_arb_trade(dbconn2):
     for row in rows:
         print(row)
         # nonKYC truncate quantity to 4 decimals
-        quant_str = str(round((row['quantity'] - 0.00005), 4))
+        quant_4f = round((row['quantity'] - 0.00005), 4)
         arb_market = row['coin'] + "_DOGE"
         active_order_list = await x.get_my_orders(status='active',limit=100, skip=0, symbol=arb_market)
         is_active_order = False
         for trade in active_order_list:
-            if ((trade['quantity'] == quant_str) and (trade['side'] == row['arb_side'])):
-                print("active order cleared: {}  quant_str: {} arb_side: {}".format(trade, quant_str, row['arb_side']))
+            if ((float(trade['quantity']) == quant_4f) and (trade['side'] == row['arb_side'])):
+                print("active order cleared: {}  quant_4f: {} arb_side: {}".format(trade, str(quant_4f), row['arb_side']))
                 hedge_side = flip_side(row['arb_side'])
                 insert_net_unhedged_record(dbconn2,row['coin'], hedge_side, row['quantity']);
                 update_remainderswap_table(dbconn2,row['coin'], row['quantity'], row['arb_price'], row['arb_side'], 2)
@@ -310,8 +310,8 @@ async def clear_cex_arb_trade(dbconn2):
         if not is_active_order:
             filled_order_list = await x.get_my_orders(status='filled',limit=100, skip=0, symbol=arb_market)
             for trade in filled_order_list:
-                if ((trade['quantity'] == quant_str) and (trade['side'] == row['arb_side'])):
-                    print("filled order cleared: {}  quant_str: {} arb_side: {}".format(trade, quant_str, row['arb_side']))
+                if ((float(trade['quantity']) == quant_4f) and (trade['side'] == row['arb_side'])):
+                    print("filled order cleared: {}  quant_4f: {} arb_side: {}".format(trade, str(quant_4f), row['arb_side']))
                     hedge_side = flip_side(row['arb_side'])
                     insert_net_unhedged_record(dbconn2,row['coin'], hedge_side, row['quantity']);
                     update_remainderswap_table(dbconn2,row['coin'], row['quantity'], row['arb_price'], row['arb_side'], 2)
