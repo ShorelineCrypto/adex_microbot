b0e6a149d45ef7e9071693c4ba30f2c6f4421ab7
ShorelineCrypto <admin@shorelinecrypto.com>
2025-12-03 21:19:24 -0500

    upgrade kdf/mm2 to v2.6.0-beta

--- mm2/update_mm2.sh
+++ mm2/update_mm2.sh
@@ -8,7 +8,7 @@ rm mm2 kdf
 # rm mm2.zip
 
 # For mm2 binaries
-wget https://github.com/ShorelineCrypto/komodo-defi-framework/releases/download/v2.5.2-beta/mm2-a295579b8-Linux-Release_amd64.tar.gz
-tar xvfz mm2-a295579b8-Linux-Release_amd64.tar.gz 
-rm mm2-a295579b8-Linux-Release_amd64.tar.gz
+wget https://github.com/GLEECBTC/komodo-defi-framework/releases/download/v2.6.0-beta/kdf_475cdb4-linux-x86-64.zip
+unzip kdf_475cdb4-linux-x86-64.zip 
+rm kdf_475cdb4-linux-x86-64.zip
 ln -s kdf mm2
--- mm2/update_mm2_arm64.sh
+++ mm2/update_mm2_arm64.sh
@@ -8,7 +8,7 @@ rm mm2 kdf
 # rm mm2.zip
 
 # For mm2 binaries
-wget https://github.com/ShorelineCrypto/komodo-defi-framework/releases/download/v2.5.2-beta/mm2-a295579b8-Linux-Release_aarch64.tar.gz
-tar xvfz mm2-a295579b8-Linux-Release_aarch64.tar.gz
-rm mm2-a295579b8-Linux-Release_aarch64.tar.gz
+wget https://github.com/ShorelineCrypto/komodo-defi-framework/releases/download/v2.6.0-beta/kdf-6c02eb83e-Linux-Release_aarch64.tar.gz
+tar xvfz kdf-6c02eb83e-Linux-Release_aarch64.tar.gz
+rm kdf-6c02eb83e-Linux-Release_aarch64.tar.gz
 ln -s kdf mm2
