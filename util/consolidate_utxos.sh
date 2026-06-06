c76174d555c3dcd2e61f822936dbf1799239458e
ShorelineCrypto <admin@shorelinecrypto.com>
2026-06-06 14:04:55 -0400

    fixed consolidate_utxos error

--- util/consolidate_utxos.sh
+++ util/consolidate_utxos.sh
@@ -5,6 +5,6 @@ if [ "$#" -ne 1 ]; then
 fi
 
 source /root/atomicDEX-API/target/debug/userpass
-curl --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"consolidate_utxos\", \"coin\":\"$1\",\"merge_conditions\":{\"merge_at\":70, \"max_merge_at_once\":50}}"
+curl --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"consolidate_utxos\",\"mmrpc\":\"2.0\",\"params\":{\"coin\":\"$1\",\"broadcast\":true,\"merge_conditions\": {\"merge_at\": 70,\"max_merge_at_once\":50}}}"
 
 echo
